bb97297b91810bf9590570a8605115cba105e0cf
Mahmudul Hasan Mahi <114220682+mahmud3301@users.noreply.github.com>
2023-03-04 22:16:52 +0600

    All Done

--- script/index.js
+++ script/index.js
@@ -15,6 +15,8 @@ const toggleSpinner = (isLoading) => {
   }
 }
 
+
+// Cards Show
 const showAll = document.getElementById('show-all');
 
 const displayAi = (ais, limit) => {
@@ -33,19 +35,19 @@ const displayAi = (ais, limit) => {
                     <h1 class="text-2xl font-bold mt-2">Features</h1><br>\
                     <div>
                         <ol class="list-decimal pl-5">
-                           <li>${ai.features[0] ? ai.features[0] : "No features"}</li>
-                           <li>${ai.features[1] ? ai.features[1] : "No features"}</li>
-                           <li>${ai.features[2] ? ai.features[2] : "No features"}</li>
-                           <li>${ai.features[3] ? ai.features[3] : "No features"}</li>
+                           <li>${ai.features[0] ? ai.features[0] : "No Data Found"}</li>
+                           <li>${ai.features[1] ? ai.features[1] : "No Data Found"}</li>
+                           <li>${ai.features[2] ? ai.features[2] : "No Data Found"}</li>
+                           <li>${ai.features[3] ? ai.features[3] : "No Data Found"}</li>
                        </ol>
                     </div><br><br>
                     <hr>
                     <div class="ml-3 mt-4 mb-7">
-                    <span class="font-bold ">${ai.name ? ai.name : "No Name"}</span><br>
+                    <span class="font-bold ">${ai.name ? ai.name : "No Data Found"}</span><br>
                     <div class="flex items-center justify-between">
                        <div class="flex gap-2">
                        <img src="images/calender.png" alt="" class="w-5">
-                       <span>${ai.published_in ? ai.published_in : "No Published Date"}</span>
+                       <span>${ai.published_in ? ai.published_in : "No Data Found"}</span>
                        </div>
                        <label for="modal" class="btn border-0" style="background-color : #FEF7F7;" onclick="loadAiDetails('${ai.id}')"><i class="fas fa-arrow-right text-error"></i></label>
                     </div>
@@ -57,7 +59,7 @@ const displayAi = (ais, limit) => {
   });
 }
 
-
+// Modal
 const loadAiDetails = async (id) => {
   const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
   const res = await fetch(url);
@@ -67,45 +69,42 @@ const loadAiDetails = async (id) => {
 
 const displayAiDetails = (data, id) => {
   const { image_link, input_output_examples, description, pricing, features, integrations, accuracy } = data;
-  // console.log(data);
-  //  const {} = data;
   const aiBody = document.getElementById("ai-body");
   aiBody.innerHTML = `
     <div class="p-5 border rounded-lg bg-red-50 border-red-300">
-      <p class="text-xl font-bold text-left">${description ? description : "No Data"}</p>
+      <p class="text-xl font-bold text-left">${description ? description : "No Data Found"}</p>
 
-      <div class="flex gap-5 flex justify-center p-3">
+      <div class="flex gap-5 justify-center p-3 grid lg:grid-cols-3 grid-cols-1">
       <div class="p-5 rounded-lg text-green-500 bg-white">
       <p class="text-center text-sm font-semibold">${pricing ? pricing[0].price : "Free Of Cost"}</p>
-      <p class="text-center text-sm font-semibold">${pricing ? pricing[0].plan : "No Plan"}</p>
+      <p class="text-center text-sm font-semibold">${pricing ? pricing[0].plan : "No Data Found"}</p>
       </div>
-      <div class="p-5 rounded-lg text-orange-500 bg-white">
+      <div class="p-5 rounded-lg text-yellow-300 bg-white">
       <p class="text-center text-sm font-semibold">${pricing ? pricing[1].price : "Free Of Cost"}</p>
-      <p class="text-center text-sm font-semibold">${pricing ? pricing[1].plan : "No Plan"}</p>
+      <p class="text-center text-sm font-semibold">${pricing ? pricing[1].plan : "No Data Found"}</p>
       </div>
       <div class="p-5 rounded-lg text-red-500 bg-white">
       <p class="text-center text-sm font-semibold">${pricing ? pricing[2].price : "Free Of Cost"}</p>
-      <p class="text-center text-sm font-semibold">${pricing ? pricing[2].plan : "No Plan"}</p>
+      <p class="text-center text-sm font-semibold">${pricing ? pricing[2].plan : "No Data Found"}</p>
       </div>
       </div>
 
-      <div class="flex justify-between p-3">
+      <div class="flex grid lg:grid-cols-2 grid-cols-1 justify-between p-3">
       <div class="p-5">
       <p class="text-xl font-bold">Features</p>
-      <li>${features ? features[1].feature_name : "No Feature"}</li>
-      <li>${features ? features[2].feature_name : "No Feature"}</li>
-      <li>${features ? features[3].feature_name : "No Feature"}</li>
+      <li>${features ? features[1].feature_name : "No Data Found"}</li>
+      <li>${features ? features[2].feature_name : "No Data Found"}</li>
+      <li>${features ? features[3].feature_name : "No Data Found"}</li>
       </div>
       <div class="p-5">
       <p class="text-xl font-bold">Integrations</p>
-      <li>${integrations ? (integrations[0] ? integrations[0] : "No Integrations") : "No Integrations"}</li>
-      <li>${integrations ? (integrations[1] ? integrations[1] : "No Integrations") : "No Integrations"}</li>
-      <li>${integrations ? (integrations[2] ? integrations[2] : "No Integrations") : "No Integrations"}</li>
-      <li>${integrations ? (integrations[3] ? integrations[3] : "No Integrations") : "No Integrations"}</li>
-      <li>${integrations ? (integrations[4] ? integrations[4] : "No Integrations") : "No Integrations"}</li>
+      <li>${integrations ? (integrations[0] ? integrations[0] : "No Data Found") : "No Data Found"}</li>
+      <li>${integrations ? (integrations[1] ? integrations[1] : "No Data Found") : "No Data Found"}</li>
+      <li>${integrations ? (integrations[2] ? integrations[2] : "No Data Found") : "No Data Found"}</li>
+      <li>${integrations ? (integrations[3] ? integrations[3] : "No Data Found") : "No Data Found"}</li>
+      <li>${integrations ? (integrations[4] ? integrations[4] : "No Data Found") : "No Data Found"}</li>
       </div>
       </div>
-
     </div>
   `
   const aiBody2 = document.getElementById("ai-body-2")
@@ -124,28 +123,24 @@ const displayAiDetails = (data, id) => {
     `;
 }
 
-
+// sort by date
 const sortBtn = document.getElementById('sort-by-date-btn');
 sortBtn.addEventListener('click', () => {
-  // document.getElementById('spinner').classList.remove('d-none');
   const url = 'https://openapi.programming-hero.com/api/ai/tools';
   fetch(url)
     .then(res => res.json())
     .then(data => {
-      // document.getElementById('spinner').classList.add('d-none');
       let aiData = data.data.tools;
       aiData.sort((a, b) => new Date(b.published_in) - new Date(a.published_in));
-      // sortData = aiData;
-      // isSort = true;
       displayAi(aiData);
     })
 });
 
-
-
-loadAi(6);
-
+// show all
 document.getElementById("show-all").addEventListener('click', function () {
   loadAi();
   showAll.classList.add('hidden');
 });
+
+loadAi(6);
+
